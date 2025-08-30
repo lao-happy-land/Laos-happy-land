0283d13671f13632a2a7c1f9111fd44aa1e72883
lamvbg <lamvbgcd191320@fpt.edu.vn>
2025-08-30 16:17:01 +0700

    fix-model-property-type

--- bds-backend/src/modules/property-type/dto/get_property_type.dto.ts
+++ bds-backend/src/modules/property-type/dto/get_property_type.dto.ts
@@ -1,5 +1,14 @@
-import { ApiPropertyOptional } from "@nestjs/swagger";
-import { PageOptionsDto } from "src/common/dtos/pageOption";
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsIn, IsOptional } from 'class-validator';
+import { PageOptionsDto } from 'src/common/dtos/pageOption';
+import { TransactionEnum } from 'src/common/enum/enum';
 
 export class GetPropertyTypeDto extends PageOptionsDto {
+  @ApiPropertyOptional({
+    description: 'Trạng thái',
+    enum: TransactionEnum,
+  })
+  @IsOptional()
+  @IsIn(Object.values(TransactionEnum))
+  transaction?: string;
 }
